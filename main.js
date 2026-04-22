f18aabfc3413dd88d3ab68f7691b44bb115cc24b
shop-Ak47 <omar67harbe@gmail.com>
2026-04-22 21:24:08 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function startMusicOnFirstInteraction() {
-  if (!isPlaying) toggleMusic();
-  document.removeEventListener('click', startMusicOnFirstInteraction);
-  document.removeEventListener('keydown', startMusicOnFirstInteraction);
-  document.removeEventListener('touchstart', startMusicOnFirstInteraction);
-}
-document.addEventListener('click', startMusicOnFirstInteraction);
-document.addEventListener('keydown', startMusicOnFirstInteraction);
-document.addEventListener('touchstart', startMusicOnFirstInteraction);
